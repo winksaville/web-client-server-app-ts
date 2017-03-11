01f93c7dcbdc988d85307572d43db705226698e4
Wink Saville <wink@saville.com>
2017-03-10 18:59:13 -0800

    Enable the shell option to true in client too.
    Seemed to work for server.spec.ts so trying it on client.spec.ts too.

--- src/client/client.spec.ts
+++ src/client/client.spec.ts
@@ -32,12 +32,12 @@ export class ClientTests {
         .build();
 
     // Start the server
-    this.server = await child.spawn('node', [ './dist/server/server.js' ]);
-    //this.server = await child.spawn('node', [ './dist/server/server.js' ], {
-    //  env: {
-    //    DEBUG: 'server'
-    //  }
-    //});
+    this.server = await child.spawn('node', [ './dist/server/server.js' ], {
+      env: {
+        DEBUG: 'server'
+      },
+      shell: true
+    });
     this.server.stdout.on('data', (data) => {
                  debug(`server.stdout: ${data}`);
     });
