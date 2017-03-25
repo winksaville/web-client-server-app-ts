0a6c7a4e88a662ded029aa08e9c6961c587f109e
Wink Saville <wink@saville.com>
2017-03-25 09:49:06 -0700

    Add chromedriver.spec
    A simpler test of using chromedriver

---
+++ src/client/chromedriver.spec.ts
@@ -0,0 +1,70 @@
+import {
+  AsyncSetupFixture,
+  AsyncTeardownFixture,
+  AsyncTest,
+  TestFixture,
+} from "alsatian";
+
+import {
+  Builder,
+  WebDriver,
+} from "selenium-webdriver";
+
+import * as chrome from "selenium-webdriver/chrome";
+
+import * as debugModule from "debug";
+
+const debug = debugModule("chromedriver.spec");
+
+const verboseLogging = true;
+
+@TestFixture("Client tests")
+export class ClientTests {
+  private driver: WebDriver;
+  private browserName = "chrome";
+
+  @AsyncSetupFixture
+  public async setupFixture() {
+    debug("setupFixture:+");
+
+    let options = new chrome.Options();
+    // options.addArguments("disable-background-timer-throttling");
+
+    if (verboseLogging) {
+      debug("setupFixture: create server and driver with verbose logging");
+      let service = new chrome.ServiceBuilder()
+          .loggingTo("./build/chromedriver.spec.log")
+          .enableVerboseLogging()
+          .build();
+
+      this.driver = chrome.Driver.createSession(options, service);
+    } else {
+      // Start the browser we"re going to control
+      debug("setupFixture: create Builder for chrome");
+      this.driver = new Builder()
+          .forBrowser(this.browserName)
+          .setChromeOptions(options)
+          .build();
+    }
+
+
+    debug("setupFixture:-");
+  }
+
+  @AsyncTeardownFixture
+  public async teardownFixture() {
+    debug("teardownFixture:+");
+
+    await this.driver.quit();
+
+    debug("teardownFixture:-");
+  }
+
+  @AsyncTest("wd: nop which is defined in a TS file")
+  public async testWdNop() {
+    debug("testWdNop:+");
+
+
+    debug("testWdNop:-");
+  }
+}
